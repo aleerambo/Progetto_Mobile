a9fec9044068b068250b439b083fde06e25f82bb
Antonino Cardillo <antonino.cardillo2@studio.unibo.it>
2025-05-31 15:09:22 +0200

    ADD GetServizi data class

---
+++ data/src/main/java/com/corsolp/data/remote/models/GetServizi.kt
@@ -0,0 +1,11 @@
+package com.corsolp.data.remote.models
+
+import com.squareup.moshi.Json
+
+
+data class GetServizi (
+
+  @Json(name = "id"            ) var id           : Int?    = null,
+  @Json(name = "nome_servizio" ) var nomeServizio : String? = null
+
+)
